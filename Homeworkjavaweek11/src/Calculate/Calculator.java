608767defa04c1fcf704f90e4ed1d15aebcad861
vinihoney <sonessel@gmail.com>
2023-11-23 01:30:22 +0000

    Homework Week11

--- Homeworkjavaweek11/src/Calculate/Calculator.java
+++ Homeworkjavaweek11/src/Calculate/Calculator.java
@@ -1,4 +1,28 @@
 package Calculate;
 
-public class Calculator {
+import java.util.Scanner;
+
+public class Calculator extends Main {
+    public static void main(String[] args) {
+
+        Main  calculator = new Main();
+        Scanner scanner = new Scanner(System.in);
+
+        // Get user input
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter the operation symbol (+, -, *, /): ");
+        char operationSymbol = scanner.next().charAt(0);
+
+        // Calculate and display result
+        calculator.calculateResult(num1, num2, operationSymbol);
+
+        // Close the scanner
+        scanner.close();
+    }
+
 }
--- Homeworkjavaweek11/src/Calculate/Main.java
+++ Homeworkjavaweek11/src/Calculate/Main.java
@@ -1,4 +1,50 @@
 package Calculate;
-
+import org.checkerframework.checker.units.qual.C;
+import java.util.Scanner;
 public class Main {
+
+    // following are Instance method for addition, Multiplication, substraction, division
+    public void addition(int a, int b) {
+        int result = a + b;
+        System.out.println("Addition Result: " + result);
+    }
+
+    public void subtraction(int a, int b) {
+        int result = a - b;
+        System.out.println("Subtraction Result: " + result);
+    }
+
+      public void multiplication(int a, int b) {
+        int result = a * b;
+        System.out.println("Multiplication Result: " + result);
+    }
+
+      public void division(int a, int b) {
+        if (b != 0) {
+            int result = a / b;
+            System.out.println("Division Result: " + result);
+        } else {
+            System.out.println("Cannot divide by zero.");
+        }
+    }
+
+    // Method to calculate result based on symbol
+    public void calculateResult(int a, int b, char symbol) {
+        switch (symbol) {
+            case '+':
+                addition(a, b);
+                break;
+            case '-':
+                subtraction(a, b);
+                break;
+            case '*':
+                multiplication(a, b);
+                break;
+            case '/':
+                division(a, b);
+                break;
+            default:
+                System.out.println("Invalid symbol. Please use '+', '-', '*', or '/'.");
+        }
+    }
 }
